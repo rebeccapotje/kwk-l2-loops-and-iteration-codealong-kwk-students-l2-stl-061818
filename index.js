4b74dc640c1f796b902ccbf74e5d69e4790cb4ea
rebeccapotje <rebecca.potje@gmail.com>
2018-06-21 16:33:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Code your solutions in this file
 
-let countdown = 10;
-
-while (countdown > 0) {
-  console.log(--countdown)
+function tailsNeverFails() {
+  let tails = 0;
+  
+  while(Math.random() >= 0.5) {
+    tails++
+  }
+  
+  return `You got ${tails} tails in a row!`;
 }
